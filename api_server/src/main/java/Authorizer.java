064319826f1525ad20591894d79dd4d65b53c279
Jeongwoo Kim - jekim <jekim@lycorp.co.jp>
2026-06-20 12:25:17 +0900

    fix
    Signed-off-by: Jeongwoo Kim - jekim <jekim@lycorp.co.jp>

--- api_server/src/main/java/Authorizer.java
+++ api_server/src/main/java/Authorizer.java
@@ -6,7 +6,6 @@ import javax.net.ssl.X509TrustManager;
 import com.sun.net.httpserver.HttpExchange;
 import com.yahoo.athenz.zpe.AuthZpeClient;
 
-import java.nio.file.Files;
 import java.security.cert.X509Certificate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -60,16 +59,6 @@ public class Authorizer {
       }
       if (System.getProperty("athenz.zpe.check_policy_zms_signature") == null) {
         System.setProperty("athenz.zpe.check_policy_zms_signature", "false");
-      }
-      if (System.getProperty("athenz.jwk_athenz_conf") == null) {
-        try {
-          java.nio.file.Path tmp = Files.createTempFile("athenz-jwk-empty", ".conf");
-          Files.writeString(tmp, "{\"zts\":{\"keys\":[]},\"zms\":{\"keys\":[]}}");
-          tmp.toFile().deleteOnExit();
-          System.setProperty("athenz.jwk_athenz_conf", tmp.toString());
-        } catch (Exception e) {
-          // non-fatal; ZPE will log a WARN but still function via jwk_uri
-        }
       }
       AuthZpeClient.init();
     }
